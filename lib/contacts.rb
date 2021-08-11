668e83774be7c5896cf5c9abf63dc0fd5d03b5d7
Christine Contreras <christineec23@yahoo.com>
2021-08-11 15:38:59 -0500

    working

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,4 +16,17 @@ require 'pry'
   
 def remove_strawberry(contacts)
 
+    contacts.each do |person, contact_details_hash|
+      if person == "Freddy Mercury"
+        contact_details_hash.each do |attribute, data|
+          if attribute == :favorite_ice_cream_flavors
+            data.delete_if {|ice_cream| ice_cream == "strawberry"}
+          end #if statement ice cream
+          
+        end #each freddy
+
+      end #if statement freddy
+
+    end #each of contacts 
+
 end
